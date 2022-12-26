c1aea7ebdb0fa5c098d750f5ecb4c3f4dc488744
Abhay Verma <abhayverma356@gmail.com>
2022-12-26 17:21:41 +0530

    first commit

---
+++ src/components/todo.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import Logo from "./images/todologo.jpg";
+import "../App.css";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteSharpIcon from "@mui/icons-material/DeleteSharp";
+
+const Todo = () => {
+  const [Data, setData] = useState("");
+  const [items, setitems] = useState([]);
+
+  const additems = () => {
+    if (!Data) {
+    } else {
+      setitems([...items, Data]);
+      setData("");
+    }
+  };
+
+  //   delete
+  const handleDelete = (id) => {
+    console.log(id);
+    const update = items.filter((element, ind) => {
+      return ind !== id;
+    });
+    setitems(update);
+  };
+
+  //   remove all
+
+  const handleRemove = () => {
+    setitems([]);
+  };
+
+  //   local storage
+
+  useEffect(()=>{
+    localStorage.setitems('lists',JSON.stringify(items));
+  }, [items]);
+
+
+// 
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img
+              src={Logo}
+              alt="logo"
+              style={{ height: "60px", width: "80px", borderRadius: "10%" }}
+            />
+            <figcaption>add your todo list</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="📝...add "
+              value={Data}
+              onChange={(e) => setData(e.target.value)}
+            />
+            <i class="fa fa-plus add-btn" title="Add Items" onClick={additems}>
+              <AddIcon />
+            </i>
+          </div>
+          <div className="showItems">
+            {items.map((element, ind) => {
+              return (
+                <div className="eachItem" key={ind}>
+                  <h3>{element}</h3>
+                  <i
+                    className="far fa-trash-alt add-btn"
+                    title="Delete Item"
+                    onClick={() => handleDelete(ind)}
+                  >
+                    <DeleteSharpIcon />
+                  </i>
+                </div>
+              );
+            })}
+            <div className="showItems">
+              <button
+                className="btn effect04"
+                data-sm-link-text="Remove All"
+                onClick={handleRemove}
+              >
+                <span> Check list</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
